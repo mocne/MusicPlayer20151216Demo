cfb207faf3a54e84e93090e6355f9e78066633a7
mocne <kaifeng0605@sina.com>
2015-12-16 20:02:23 +0800

    Create the project.

---
+++ MusicPlayerDemo/MusicPlayerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MusicPlayerDemo
+//
+//  Created by qingyun on 15/12/16.
+//  Copyright © 2015年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
