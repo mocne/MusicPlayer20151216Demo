e758e2c7b51fbb08ec7404b9047b12c44688b106
mocne <kaifeng0605@sina.com>
2015-12-17 08:47:46 +0800

    player interface

--- MusicPlayerDemo/MusicPlayerDemo/ListViewController.h
+++ MusicPlayerDemo/MusicPlayerDemo/ListViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ListViewController.h
 //  MusicPlayerDemo
 //
 //  Created by qingyun on 15/12/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ListViewController : UIViewController
 
 @end
-
---
+++ MusicPlayerDemo/MusicPlayerDemo/PlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayViewController.h
+//  MusicPlayerDemo
+//
+//  Created by qingyun on 15/12/16.
+//  Copyright © 2015年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayViewController : UIViewController
+
+@end
